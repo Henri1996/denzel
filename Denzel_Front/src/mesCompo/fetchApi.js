341a0ddfffa0bff925dc2b5aa090a0bec54d3af0
Prenom NOM <nom.prenom@fai.fr>
2019-03-18 12:11:56 +0100

    update

---
+++ Denzel_Front/src/mesCompo/fetchApi.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+import { Table } from 'reactstrap';
+
+
+
+class fetchApi extends Component {
+  
+    constructor(props){
+        super(props);
+        this.state = {
+          item : [],
+          isLoaded: false,
+        }
+      }
+  componentDidMount(){
+    fetch('http://localhost:9292/Movie')
+    .then(res => res.json())
+    .then(json =>{
+        this.setState({
+          isLoaded : true,
+          item : json,
+        })
+    }
+      
+    )
+
+}
+  render() {
+    var { isLoaded , item} = this.state
+
+    if(!isLoaded){
+      return <div>Loading....</div>
+    }
+    else{
+    return (
+      <div>
+        <ul>
+          {item.map(it=>(<li>
+            Title : {it.title}
+          </li>))}
+        </ul>
+        </div>
+    );
+  }
+  }
+}
+export default fetchApi;
